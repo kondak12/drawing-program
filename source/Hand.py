98a9b50a0e00b041c5379b31df6145e55cd90183
IvanPC <ivan.kondakov.98@bk.ru>
2025-07-23 23:44:11 +0300

    feature(Hand) class Hand was added

---
+++ source/Hand.py
@@ -0,0 +1,36 @@
+import pygame
+
+import Instruments
+from configs import colors
+
+
+class Hand:
+
+    def __init__(self):
+        self.__main_instrument = Instruments.BrushTool()
+        self.__main_color = colors.BLACK
+        self.__line_size = 10
+
+    def position(self):
+        return pygame.mouse.get_pos()
+
+    def draw(self):
+        self.__main_instrument.draw()
+
+    def get_main_color(self) -> colors.color:
+        return self.__main_color
+
+    def get_main_instrument(self) -> Instruments.Instrument:
+        return self.__main_instrument
+
+    def get_line_size(self) -> int:
+        return self.__line_size
+
+    def set_main_color(self, new_color: colors.color) -> None:
+        self.__main_color = new_color
+
+    def set_main_instrument(self, new_instrument: Instruments.Instrument) -> None:
+        self.__main_instrument = new_instrument
+
+    def set_line_size(self, new_size: int) -> None:
+        self.__line_size = new_size
