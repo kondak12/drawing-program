83ffaed77560fcbb9f9b290fa6f8247cc8486247
IvanPC <ivan.kondakov.98@bk.ru>
2025-07-23 19:54:18 +0300

    feature(source) MainLoop class was added

---
+++ source/MainLoop.py
@@ -0,0 +1,21 @@
+import pygame
+import sys
+
+
+class MainLoop:
+
+    def __init__(self):
+        self.__running = True
+
+    def run(self) -> None:
+        while self.__running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+    def get_running(self) -> bool:
+        return self.__running
+
+    def set_running(self, new_status: bool) -> None:
+        self.__running = new_status
