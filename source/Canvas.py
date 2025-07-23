90460a9958fe2c71fffd69a4cf0cdb01116a79af
IvanPC <ivan.kondakov.98@bk.ru>
2025-07-23 23:52:27 +0300

    feature(Hand) Hand class was added

---
+++ source/Canvas.py
@@ -0,0 +1,23 @@
+import pygame.display
+
+import configs.main_settings
+
+
+class Canvas:
+
+    def __init__(self):
+        self.__canvas_borders = configs.main_settings.CANVAS_BORDERS
+        self.__screenshot_box = configs.main_settings.SCREENSHOT_BOX
+        self.__example = ...
+
+    def __screenshot(self, canvas, path: configs.main_settings.SAVE_SCREENSHOT_PATH) -> None:
+        ...
+
+    def render(self) -> None:
+        pygame.display.flip()
+
+    def get_canvas_borders(self) -> (int, int):
+        return self.__canvas_borders
+
+    def set_canvas_borders(self, new_borders: (int, int)) -> None:
+        self.__canvas_borders = new_borders
