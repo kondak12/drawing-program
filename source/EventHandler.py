970bc8d8656cf883779dac093846bc0f582ff855
IvanPC <ivan.kondakov.98@bk.ru>
2025-07-23 21:51:16 +0300

    refactor(EventHandler) EventHandler class was edited

--- source/EventHandler.py
+++ source/EventHandler.py
@@ -1,10 +1,3 @@
-from source.MainLoop import MainLoop
-
-
 class EventHandler:
 
-    def __init__(self, main_loop: MainLoop):
-        self.__main_loop = main_loop
-
-    def stop_running(self) -> None:
-        self.__main_loop.set_running(False)
+    ...
