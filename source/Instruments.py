d59e53c071bd517c9e7c65a829c45b2470405ce7
IvanPC <ivan.kondakov.98@bk.ru>
2025-07-24 13:42:03 +0300

    feature(BrushTool) add class's operation

--- source/Instruments.py
+++ source/Instruments.py
@@ -1,7 +1,14 @@
 from abc import ABC, abstractmethod
+import pygame
 
 
 class Instrument(ABC):
+
+    def __init__(self, display, draw_color, mouse_pos, draw_radius):
+        self.display = display
+        self.draw_color = draw_color
+        self.mouse_pos = mouse_pos
+        self.draw_radius = draw_radius
 
     @abstractmethod
     def draw(self) -> None:
@@ -10,5 +17,20 @@ class Instrument(ABC):
 
 class BrushTool(Instrument):
 
+    def __init__(self, display, draw_color, mouse_pos, draw_radius, sprite_path=None):
+        super().__init__(display, draw_color, mouse_pos, draw_radius)
+        self.__sprite_path = sprite_path
+
+        if self.__sprite_path is not None:
+            self.__sprite_size = pygame.image.load(self.__sprite_path).get_rect().size
+
     def draw(self) -> None:
-        pass
+
+        if pygame.mouse.get_pressed()[0] and self.__sprite_path is None:
+            pygame.draw.circle(self.display, self.draw_color, self.mouse_pos, self.draw_radius)
+
+        elif pygame.mouse.get_pressed()[0] and self.__sprite_path is not None:
+            self.display.blit(self.__sprite_path,
+                              (self.mouse_pos[0] - (self.__sprite_size[0] // 2),
+                               self.mouse_pos[1] - self.__sprite_size[1] // 2)
+                              )
