17b4f94e1c61f33266be299654471201516190af
IvanPC <ivan.kondakov.98@bk.ru>
2025-07-28 15:48:47 +0300

    style(MainClass) make code more solid

--- source/MainClass.py
+++ source/MainClass.py
@@ -1,20 +1,19 @@
 import pygame
 
-import configs.colors
-from configs.main_settings import SCREEN_SIZE
+from configs import colors, main_settings
 from source.MainLoop import MainLoop
 
 
 class MainClass:
 
     def __init__(self):
-        self.__screen = pygame.display.set_mode(size=SCREEN_SIZE)
+        self.__screen = pygame.display.set_mode(size=main_settings.SCREEN_SIZE)
         self.__main_loop = MainLoop(self.__screen)
 
     def run(self) -> None:
         pygame.init()
 
-        self.__screen.fill(configs.colors.WHITE)
+        self.__screen.fill(colors.WHITE)
         self.__main_loop.run()
 
     def set_screen_size(self, new_size: (int, int)) -> None:
