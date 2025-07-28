305729272827d1d2273b1aa5c82d42407c65c400
IvanPC <ivan.kondakov.98@bk.ru>
2025-07-28 15:58:37 +0300

    refactor(gui) delete 'color' field

--- source/gui.py
+++ source/gui.py
@@ -3,14 +3,13 @@ import pygame
 
 class Button:
 
-    def __init__(self, width, height, display, color=None, picture=None):
+    def __init__(self, width, height, display, picture=None):
         self.__width = width
         self.__height = height
         self.__display = display
-        self.__color = color
         self.__picture = picture
 
-    def draw(self, x_pos: int, y_pos: int, mouse_pos: (int, int), function) -> None:
+    def draw(self, x_pos: int, y_pos: int, mouse_pos: (int, int), color, function) -> None:
 
         if x_pos < mouse_pos[0] < x_pos + self.__width and y_pos < mouse_pos[1] < y_pos + self.__height:
 
@@ -19,5 +18,5 @@ class Button:
 
         else:
             pygame.draw.rect(self.__display,
-                             self.__color,
+                             color,
                              pygame.Rect(x_pos, y_pos, self.__width, self.__height))
