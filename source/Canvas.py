30ad7012e2a6856b6bc2dc0ad39ceab1acc0f458
IvanPC <ivan.kondakov.98@bk.ru>
2025-07-28 15:43:47 +0300

    feature(Canvas) add new fields and method for class

--- source/Canvas.py
+++ source/Canvas.py
@@ -1,16 +1,26 @@
 import pygame
 
-import configs.main_settings
+from configs import main_settings, colors
+from source import gui, Hand
 
 
 class Canvas:
 
-    def __init__(self):
-        self.__canvas_borders = configs.main_settings.CANVAS_BORDERS
-        self.__screenshot_box = configs.main_settings.SCREENSHOT_BOX
-        self.__example = ...
+    def __init__(self, display, hand: Hand):
+        self.__hand = hand
+        self.__display = display
+        self.__canvas_borders = main_settings.CANVAS_BORDERS
+        self.__screenshot_box = main_settings.SCREENSHOT_BOX
+        self.__button_example = gui.Button(50, 50, self.__display, colors.RED)
 
-    def __screenshot(self, canvas, path: configs.main_settings.SAVE_SCREENSHOT_PATH) -> None:
+    def place_button(self) -> None:
+        self.__button_example.draw(main_settings.SCREEN_SIZE[0] - 50,
+                                   main_settings.SCREEN_SIZE[1] + 50,
+                                   pygame.mouse.get_pos(),
+                                   lambda: self.__hand.set_draw_color(colors.RED)
+                                   )
+
+    def __screenshot(self, canvas, path: main_settings.SAVE_SCREENSHOT_PATH) -> None:
         ...
 
     def get_canvas_borders(self) -> (int, int):
