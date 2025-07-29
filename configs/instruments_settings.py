1835ac5c4cd2b178aee83d0db133683c7350183d
IvanPC <ivan.kondakov.98@bk.ru>
2025-07-29 23:49:21 +0300

    feature(instruments_settings) add new constants

--- configs/instruments_settings.py
+++ configs/instruments_settings.py
@@ -1,2 +1,7 @@
+BRUSH_TOOL = "BrushTool"
+FILL_TOOL = "FillTool"
+PATTERN_TOOL_CIRCLE = "PatternToolCircle"
+PATTERN_TOOL_RECT = "PatternToolRect"
+
 PATTERN_TYPE_CIRCLE = 'circle'
 PATTERN_TYPE_RECT = 'rect'
