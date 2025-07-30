68c06b5b289cf7128a6b8234f9aa0f3866db4a55
IvanPC <ivan.kondakov.98@bk.ru>
2025-07-31 01:06:43 +0300

    feature(colors) add new colors

--- configs/colors.py
+++ configs/colors.py
@@ -5,3 +5,16 @@ WHITE = color.THECOLORS['white']
 BLACK = color.THECOLORS['black']
 
 RED = color.THECOLORS['red']
+
+GREEN = color.THECOLORS['green']
+
+BLUE = color.THECOLORS['blue']
+
+PURPLE = color.THECOLORS['purple']
+
+YELLOW = color.THECOLORS['yellow']
+
+ORANGE = color.THECOLORS['orange']
+
+
+COLORS = [WHITE, BLACK, RED, GREEN, BLUE]
