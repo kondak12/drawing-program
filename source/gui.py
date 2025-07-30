3115ae4354a0683db923df7a564e7a01ff2a563a
IvanPC <ivan.kondakov.98@bk.ru>
2025-07-31 02:54:29 +0300

    refactor(gui) now it's mooore flexible

--- source/gui.py
+++ source/gui.py
@@ -1,5 +1,7 @@
 import os, pygame
 
+from configs.main_settings import CANVAS_BORDERS
+
 
 class Button:
 
@@ -10,7 +12,7 @@ class Button:
 
     def draw_color(self, x_pos: int, y_pos: int, mouse_pos: (int, int), color, function) -> None:
 
-        if x_pos < mouse_pos[0] < x_pos + self.__width and y_pos < mouse_pos[1] < y_pos + self.__height:
+        if (x_pos + CANVAS_BORDERS[0]) < mouse_pos[0] < (x_pos + self.__width + CANVAS_BORDERS[0]) and y_pos < mouse_pos[1] < (y_pos + self.__height):
 
             if pygame.mouse.get_pressed()[0]:
                 function()
@@ -22,7 +24,7 @@ class Button:
 
     def draw_instrument(self, x_pos: int, y_pos: int, mouse_pos: (int, int), img_name: str, function) -> None:
 
-        if x_pos < mouse_pos[0] < x_pos + self.__width and y_pos < mouse_pos[1] < y_pos + self.__height:
+        if (x_pos + CANVAS_BORDERS[0]) < mouse_pos[0] < (x_pos + self.__width + CANVAS_BORDERS[0]) and y_pos < mouse_pos[1] < (y_pos + self.__height):
 
             if pygame.mouse.get_pressed()[0]:
                 icon = pygame.image.load(os.path.join(r"resources\imgs", f"{img_name + '_grey.jpg'}"))
