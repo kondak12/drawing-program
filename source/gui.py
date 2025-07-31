f10e5336dfeb4aa0270d2843b16c5ee5575bff10
IvanPC <ivan.kondakov.98@bk.ru>
2025-07-31 16:50:27 +0300

    fix(gui) buttons of choice instruments and colors are fixed

--- source/gui.py
+++ source/gui.py
@@ -17,20 +17,20 @@ class Button:
             if pygame.mouse.get_pressed()[0]:
                 function()
 
-        else:
-            pygame.draw.rect(self.__display,
-                             color,
-                             pygame.Rect(x_pos, y_pos, self.__width, self.__height))
+        pygame.draw.rect(self.__display,
+                         color,
+                         pygame.Rect(x_pos, y_pos, self.__width, self.__height))
 
     def draw_instrument(self, x_pos: int, y_pos: int, mouse_pos: (int, int), img_name: str, function) -> None:
 
         if (x_pos + CANVAS_BORDERS[0]) < mouse_pos[0] < (x_pos + self.__width + CANVAS_BORDERS[0]) and y_pos < mouse_pos[1] < (y_pos + self.__height):
 
+            icon = pygame.image.load(os.path.join(r"resources\imgs", f"{img_name + '_grey.jpg'}"))
+
             if pygame.mouse.get_pressed()[0]:
-                icon = pygame.image.load(os.path.join(r"resources\imgs", f"{img_name + '_grey.jpg'}"))
-                self.__display.blit(icon, (x_pos, y_pos))
                 function()
 
         else:
             icon = pygame.image.load(os.path.join(r"resources\imgs", f"{img_name + '_black.jpg'}"))
-            self.__display.blit(icon, (x_pos, y_pos))
+
+        self.__display.blit(icon, (x_pos, y_pos))
