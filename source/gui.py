e4599fae0ef34ebc68a96512ff0b00a45b17d18f
IvanPC <ivan.kondakov.98@bk.ru>
2025-08-01 17:10:07 +0300

    refactor(gui) update buttons check type

--- source/gui.py
+++ source/gui.py
@@ -10,25 +10,27 @@ class Button:
         self.__height = height
         self.__display = display
 
-    def draw_color(self, x_pos: int, y_pos: int, mouse_pos: (int, int), color, function) -> None:
+    def draw_color(self, x_pos: int, y_pos: int, mouse_pos: (int, int), color, event, function) -> None:
 
         if (x_pos + CANVAS_BORDERS[0]) < mouse_pos[0] < (x_pos + self.__width + CANVAS_BORDERS[0]) and y_pos < mouse_pos[1] < (y_pos + self.__height):
 
-            if pygame.mouse.get_pressed()[0]:
-                function()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == pygame.BUTTON_LEFT:
+                    function()
 
         pygame.draw.rect(self.__display,
                          color,
                          pygame.Rect(x_pos, y_pos, self.__width, self.__height))
 
-    def draw_instrument(self, x_pos: int, y_pos: int, mouse_pos: (int, int), img_name: str, function) -> None:
+    def draw_instrument(self, x_pos: int, y_pos: int, mouse_pos: (int, int), img_name: str, event, function) -> None:
 
         if (x_pos + CANVAS_BORDERS[0]) < mouse_pos[0] < (x_pos + self.__width + CANVAS_BORDERS[0]) and y_pos < mouse_pos[1] < (y_pos + self.__height):
 
             icon = pygame.image.load(os.path.join(r"resources\imgs", f"{img_name + '_grey.jpg'}"))
 
-            if pygame.mouse.get_pressed()[0]:
-                function()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == pygame.BUTTON_LEFT:
+                    function()
 
         else:
             icon = pygame.image.load(os.path.join(r"resources\imgs", f"{img_name + '_black.jpg'}"))
