23ed1f30e431828b1a51f6e7310b589ad27efef6
IvanPC <ivan.kondakov.98@bk.ru>
2025-08-04 16:46:11 +0300

    feature(instruments_settings) add new buttons dict

--- configs/instruments_settings.py
+++ configs/instruments_settings.py
@@ -5,3 +5,9 @@ PATTERN_TOOL_RECT = "PatternToolRect"
 
 PATTERN_TYPE_CIRCLE = 'circle'
 PATTERN_TYPE_RECT = 'rect'
+
+DRAWING_RADIUS_BUTTON_DICT = {
+    1: ["1_draw_radius", 5],
+    2: ["2_draw_radius", 15],
+    3: ["3_draw_radius", 30]
+}
