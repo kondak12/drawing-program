195de349e239ab660d1db8485fac7f64cb45b95e
IvanPC <ivan.kondakov.98@bk.ru>
2025-08-11 00:35:09 +0300

    refactor/feature(gui) mass refactor and add show current color/instrument

--- source/gui.py
+++ source/gui.py
@@ -1,77 +1,129 @@
 import pygame
 
-from configs.main_settings import CANVAS_BORDERS, MAIN_COLOR_SHOW_COORDINATES, MAIN_INSTRUMENT_SHOW_COORDINATES
+from abc import ABC, abstractmethod
+
+from configs.main_settings import CANVAS_SIZE, MAIN_COLOR_SHOW_COORDINATES, MAIN_INSTRUMENT_SHOW_COORDINATES
 from configs.instruments_settings import LINE_SIZE
 from configs.images import BUTTONS
 
 
-class Button:
+class GUIZone:
 
-    def __init__(self, width, height, favorable_region, canvas):
+    def __init__(self, canvas, width, height):
+        self.__canvas = canvas
         self.__width = width
         self.__height = height
-        self.__favorable_region = favorable_region
-        self.__canvas = canvas
-        self.__current_line_size_index = 0
+        self.__box_size = (width, height)
 
-    def __in_box(self, x_pos: int, y_pos: int) -> None:
-        return ((x_pos + CANVAS_BORDERS[0]) < self.__canvas.get_hand().get_mouse_pos()[0] < (x_pos + self.__width + CANVAS_BORDERS[0])
-                and y_pos < self.__canvas.get_hand().get_mouse_pos()[1] < (y_pos + self.__height))
+    def place(self, place_coord: tuple) -> None:
+        pygame.draw.rect(
+            self.__canvas,
+            pygame.color.THECOLORS['grey'],
+            pygame.Rect(place_coord, self.__box_size)
+        )
 
-    def __is_press(self, event: pygame.event) -> bool:
+
+class AbstractButton(ABC):
+    def __init__(self, width, height, favorable_region_surface, canvas_manager):
+        self._width = width
+        self._height = height
+        self._favorable_region_surface = favorable_region_surface
+        self._canvas_manager = canvas_manager
+
+    def _in_box(self, x_pos: int, y_pos: int) -> bool:
+        mouse_x, mouse_y = self._canvas_manager.get_hand().get_mouse_pos()
+        left_gui_border = x_pos + CANVAS_SIZE[0]
+        right_gui_border = x_pos + self._width + CANVAS_SIZE[0]
+        upper_gui_border = y_pos
+        lower_gui_border = y_pos + self._height
+
+        return left_gui_border < mouse_x < right_gui_border and upper_gui_border < mouse_y < lower_gui_border
+
+    def _is_press(self, event: pygame.event) -> bool:
         return event.type == pygame.MOUSEBUTTONDOWN and event.button == pygame.BUTTON_LEFT
 
-    def show_users_uses(self) -> None:
+    @abstractmethod
+    def place(self, x_pos: int, y_pos: int, event, **kwargs) -> None:
+        pass
+
+
+class ColorButton(AbstractButton):
+    def __init__(self, width, height, favorable_region_surface, canvas_manager):
+        super().__init__(width, height, favorable_region_surface, canvas_manager)
+
+    def place_current_color(self) -> None:
         pygame.draw.rect(
-            self.__favorable_region,
-            self.__canvas.get_hand().get_main_color(),
+            self._favorable_region_surface,
+            self._canvas_manager.get_hand().get_main_color(),
             pygame.Rect(MAIN_COLOR_SHOW_COORDINATES)
         )
 
-    def color(self, x_pos: int, y_pos: int, color, event) -> None:
-
-        if self.__in_box(x_pos, y_pos) and self.__is_press(event):
-            self.__canvas.get_hand().set_main_color(color)
+    def place(self, x_pos: int, y_pos: int, event, color=None) -> None:
+        if self._in_box(x_pos, y_pos) and self._is_press(event):
+            self._canvas_manager.get_hand().set_main_color(color)
 
         pygame.draw.rect(
-            self.__favorable_region,
+            self._favorable_region_surface,
             color,
-            pygame.Rect(x_pos, y_pos, self.__width, self.__height)
+            pygame.Rect(x_pos, y_pos, self._width, self._height)
         )
 
-    def instrument(self, x_pos: int, y_pos: int, caps_img_name: str, event) -> None:
 
-        if self.__in_box(x_pos, y_pos):
+class InstrumentButton(AbstractButton):
+
+    def __init__(self, width, height, favorable_region_surface, canvas_manager):
+        super().__init__(width, height, favorable_region_surface, canvas_manager)
+
+    def place_current_instrument(self) -> None:
+        current_instrument = self._canvas_manager.get_hand().get_main_instrument_type()
+
+        self._favorable_region_surface.blit(
+            BUTTONS[current_instrument][0],
+            MAIN_INSTRUMENT_SHOW_COORDINATES
+        )
+
+    def place(self, x_pos: int, y_pos: int, event, caps_img_name=None, **kwargs) -> None:
+        if self._in_box(x_pos, y_pos):
             icon = BUTTONS[f"{caps_img_name}"][1]
 
-            if self.__is_press(event):
-                    self.__canvas.get_hand().set_main_instrument(caps_img_name.lower())
+            if self._is_press(event):
+                self._canvas_manager.get_hand().set_main_instrument(caps_img_name)
+
         else:
             icon = BUTTONS[f"{caps_img_name}"][0]
 
-        self.__favorable_region.blit(icon, (x_pos, y_pos))
+        self._favorable_region_surface.blit(icon, (x_pos, y_pos))
 
-    def line_size(self, x_pos: int, y_pos: int, event) -> None:
 
-        if self.__in_box(x_pos, y_pos) and self.__is_press(event):
+class LineSizeButton(AbstractButton):
 
-            if self.__current_line_size_index == 2:
-                self.__current_line_size_index = 0
-            else:
-                self.__current_line_size_index += 1
+    def __init__(self, width, height, favorable_region_surface, canvas_manager):
+        super().__init__(width, height, favorable_region_surface, canvas_manager)
+        self._current_line_size_index = 0
 
-            self.__canvas.get_hand().set_line_size(LINE_SIZE[self.__current_line_size_index])
+    def place(self, x_pos: int, y_pos: int, event, **kwargs) -> None:
+        if self._in_box(x_pos, y_pos) and self._is_press(event):
+            self._current_line_size_index = (self._current_line_size_index + 1) % 3
+            self._canvas_manager.get_hand().set_line_size(LINE_SIZE[self._current_line_size_index])
 
-        icon = BUTTONS["LINE_SIZE"][self.__current_line_size_index]
-        self.__favorable_region.blit(icon, (x_pos, y_pos))
+        icon = BUTTONS["LINE_SIZE"][self._current_line_size_index]
+        self._favorable_region_surface.blit(icon, (x_pos, y_pos))
 
-    def other_function(self, x_pos: int, y_pos: int, img_name, event, function) -> None:
-        if self.__in_box(x_pos, y_pos):
+
+class FunctionButton(AbstractButton):
+
+    def __init__(self, width, height, favorable_region_surface, canvas_manager):
+        super().__init__(width, height, favorable_region_surface, canvas_manager)
+
+    def place(self, x_pos: int, y_pos: int, event, img_name=None, function=None, **kwargs) -> None:
+
+        if self._in_box(x_pos, y_pos):
             icon = BUTTONS[f"{img_name}"][1]
 
-            if self.__is_press(event):
-                    function()
+            if self._is_press(event):
+                function()
+
         else:
             icon = BUTTONS[f"{img_name}"][0]
 
-        self.__favorable_region.blit(icon, (x_pos, y_pos))
+        self._favorable_region_surface.blit(icon, (x_pos, y_pos))
